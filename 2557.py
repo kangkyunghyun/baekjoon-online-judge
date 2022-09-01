db10f213ae2cd78682909a51bd6a223258de4480
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2022-09-02 01:08:27 +0900

    fix: 2557 description

--- 2557.py
+++ 2557.py
@@ -1,12 +1,12 @@
 # 2557: Hello World
 #
-# 문제
-# - Hello World!를 출력하시오.
+# - 문제
+# Hello World!를 출력하시오.
 #
-# 입력
-# - 없음
+# - 입력
+# 없음
 #
-# 출력
-# - Hello World!를 출력하시오.
+# - 출력
+# Hello World!를 출력하시오.
 
 print("Hello World!")
