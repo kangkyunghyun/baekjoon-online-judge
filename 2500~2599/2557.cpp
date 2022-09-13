c6c1bffdcf43ef77b1799598bdba4517da98c0e9
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2022-09-13 22:59:30 +0900

    feat: (c++) 2557 Hello World

---
+++ 2500~2599/2557.cpp
@@ -0,0 +1,17 @@
+// 2557: Hello World
+//
+// - 문제
+// Hello World!를 출력하시오.
+//
+// - 입력
+// 없음
+//
+// - 출력
+// Hello World!를 출력하시오.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+}
