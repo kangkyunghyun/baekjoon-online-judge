11d452695deac5d8d26188fb64de4e0dbc66c5c3
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2022-09-14 17:49:57 +0900

    fix: 3733 shares 설명 추가

--- 3700~3799/3733.cpp
+++ 3700~3799/3733.cpp
@@ -1,5 +1,6 @@
 // 3733: Shares
 //
+// - 문제
 // A group of N persons and the ACM Chief Judge share equally a number of S shares (not necessary all of them).
 // Let x be the number of shares aquired by each person (x must be an integer).
 // The problem is to compute the maximum value of x.
