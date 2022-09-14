b2d51b98f6d03b6c8919db999287ecd6255f1648
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2022-09-15 00:48:01 +0900

    feat: (c++) 10170 NFC West vs North

---
+++ 10100~10199/10170.cpp
@@ -0,0 +1,29 @@
+// 10170: NFC West vs North
+//
+// - 문제
+// 동혁이를 위해 NFC 서부와 북부 디비전 순위를 출력하는 프로그램을 작성하시오.
+//
+// - 입력
+// 없음
+//
+// - 출력
+// 예제와 같이 NFC 서부와 북부 디비전 순위를 출력한다.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "NFC West       W   L  T" << '\n';
+  cout << "-----------------------" << '\n';
+  cout << "Seattle        13  3  0" << '\n';
+  cout << "San Francisco  12  4  0" << '\n';
+  cout << "Arizona        10  6  0" << '\n';
+  cout << "St. Louis      7   9  0\n"
+       << '\n';
+  cout << "NFC North      W   L  T" << '\n';
+  cout << "-----------------------" << '\n';
+  cout << "Green Bay      8   7  1" << '\n';
+  cout << "Chicago        8   8  0" << '\n';
+  cout << "Detroit        7   9  0" << '\n';
+  cout << "Minnesota      5  10  1" << '\n';
+}
