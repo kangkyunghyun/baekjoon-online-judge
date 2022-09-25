a238c862ee708859a213a5eaf8da7172a505d4d6
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2022-09-25 17:00:23 +0900

    feat: (c++) 2445 별 찍기 - 8

---
+++ 2400~2499/2445.cpp
@@ -0,0 +1,43 @@
+// 2445: 별 찍기 - 8
+//
+// - 문제
+// 예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
+//
+// - 입력
+// 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+//
+// - 출력
+// 첫째 줄부터 2×N-1번째 줄까지 차례대로 별을 출력한다.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i + 1; j++) {
+      cout << '*';
+    }
+    for (int j = 0; j < 2 * (n - i - 1); j++) {
+      cout << ' ';
+    }
+    for (int j = 0; j < i + 1; j++) {
+      cout << '*';
+    }
+    cout << '\n';
+  }
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      cout << '*';
+    }
+    for (int j = 0; j < 2 * (i + 1); j++) {
+      cout << ' ';
+    }
+    for (int j = 0; j < n - i - 1; j++) {
+      cout << '*';
+    }
+    cout << '\n';
+  }
+  return 0;
+}
