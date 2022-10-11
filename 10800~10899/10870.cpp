07af21e37b531216df42346d108c5c500ef6e58a
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2022-10-11 10:50:44 +0900

    refac: (c++) 10870 피보나치 수 5 (재귀)

--- 10800~10899/10870.cpp
+++ 10800~10899/10870.cpp
@@ -16,22 +16,17 @@
 #include <iostream>
 using namespace std;
 
+int fibonacci(int n) {
+  if (n == 0)
+    return 0;
+  else if (n == 1)
+    return 1;
+  else
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
 int main() {
   int n;
   cin >> n;
-  if (n == 0) {
-    cout << 0 << '\n';
-  } else if (n == 1) {
-    cout << 1 << '\n';
-  } else {
-    int a = 0, b = 1;
-    for (int i = 1; i <= n; i++) {
-      int temp;
-      temp = a + b;
-      a = b;
-      b = temp;
-    }
-    cout << a << '\n';
-  }
+  cout << fibonacci(n) << '\n';
   return 0;
 }
