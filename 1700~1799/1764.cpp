2136e365a514d07b633db09b80d42989900fd282
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2022-10-11 18:38:47 +0900

    feat: (c++) 1764 듣보잡

---
+++ 1700~1799/1764.cpp
@@ -0,0 +1,40 @@
+// 1764: 듣보잡
+//
+// - 문제
+// 김진영이 듣도 못한 사람의 명단과, 보도 못한 사람의 명단이 주어질 때, 듣도 보도 못한 사람의 명단을 구하는 프로그램을 작성하시오.
+//
+// - 입력
+// 첫째 줄에 듣도 못한 사람의 수 N, 보도 못한 사람의 수 M이 주어진다. 이어서 둘째 줄부터 N개의 줄에 걸쳐 듣도 못한 사람의 이름과, N+2째 줄부터 보도 못한 사람의 이름이 순서대로 주어진다.
+// 이름은 띄어쓰기 없이 알파벳 소문자로만 이루어지며, 그 길이는 20 이하이다. N, M은 500,000 이하의 자연수이다.
+// 듣도 못한 사람의 명단에는 중복되는 이름이 없으며, 보도 못한 사람의 명단도 마찬가지이다.
+//
+// - 출력
+// 듣보잡의 수와 그 명단을 사전순으로 출력한다.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, m, cnt = 0;
+  cin >> n >> m;
+  map<string, bool> listen;
+  vector<string> v;
+  for (int i = 0; i < n; i++) {
+    string s;
+    cin >> s;
+    listen.insert(make_pair(s, true));
+  }
+  for (int i = 0; i < m; i++) {
+    string s;
+    cin >> s;
+    if (listen[s]) {
+      cnt++;
+      v.push_back(s);
+    }
+  }
+  sort(v.begin(), v.end());
+  cout << cnt << '\n';
+  for (string elem : v)
+    cout << elem << '\n';
+  return 0;
+}
