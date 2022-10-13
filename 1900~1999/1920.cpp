8b2567b84ad9c8599250f9b3b62721e7fc44855a
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2022-10-13 20:50:41 +0900

    feat: (c++) 1920 수 찾기

---
+++ 1900~1999/1920.cpp
@@ -0,0 +1,31 @@
+// 1920: 수 찾기
+//
+// - 문제
+// N개의 정수 A[1], A[2], …, A[N]이 주어져 있을 때, 이 안에 X라는 정수가 존재하는지 알아내는 프로그램을 작성하시오.
+//
+// - 입력
+// 첫째 줄에 자연수 N(1 ≤ N ≤ 100,000)이 주어진다. 다음 줄에는 N개의 정수 A[1], A[2], …, A[N]이 주어진다. 다음 줄에는 M(1 ≤ M ≤ 100,000)이 주어진다.
+// 다음 줄에는 M개의 수들이 주어지는데, 이 수들이 A안에 존재하는지 알아내면 된다. 모든 정수의 범위는 -2^31 보다 크거나 같고 2^31보다 작다.
+//
+// - 출력
+// M개의 줄에 답을 출력한다. 존재하면 1을, 존재하지 않으면 0을 출력한다.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  int n, m, x;
+  cin >> n;
+  vector<int> v(n);
+  for (int i = 0; i < n; i++)
+    cin >> v[i];
+  sort(v.begin(), v.end());
+  cin >> m;
+  for (int i = 0; i < m; i++) {
+    cin >> x;
+    cout << binary_search(v.begin(), v.end(), x) << '\n';
+  }
+  return 0;
+}
