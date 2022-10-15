68ee0a3ba0650ff5faccfe733ffe889c128739e8
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2022-10-15 14:25:36 +0900

    feat: (c++) 11656 접미사 배열

---
+++ 11600~11699/11656.cpp
@@ -0,0 +1,31 @@
+// 11656: 접미사 배열
+//
+// - 문제
+// 접미사 배열은 문자열 S의 모든 접미사를 사전순으로 정렬해 놓은 배열이다.
+// baekjoon의 접미사는 baekjoon, aekjoon, ekjoon, kjoon, joon, oon, on, n 으로 총 8가지가 있고, 이를 사전순으로 정렬하면, aekjoon, baekjoon, ekjoon, joon, kjoon, n, on, oon이 된다.
+// 문자열 S가 주어졌을 때, 모든 접미사를 사전순으로 정렬한 다음 출력하는 프로그램을 작성하시오.
+//
+// - 입력
+// 첫째 줄에 문자열 S가 주어진다. S는 알파벳 소문자로만 이루어져 있고, 길이는 1,000보다 작거나 같다.
+//
+// - 출력
+// 첫째 줄부터 S의 접미사를 사전순으로 한 줄에 하나씩 출력한다.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  string s;
+  cin >> s;
+  vector<string> v;
+  for (int i = 0; i < s.size(); i++) {
+    string ss = s.substr(i, s.size() - i);
+    v.push_back(ss);
+  }
+  sort(v.begin(), v.end());
+  for (string i : v)
+    cout << i << '\n';
+  return 0;
+}
