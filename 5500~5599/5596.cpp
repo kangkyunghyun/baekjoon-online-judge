2716053683104b5c19c782b867344a69670987ec
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2022-12-06 00:16:48 +0900

    fix: (c++) 5596 change number

--- 5500~5599/5596.cpp
+++ 5500~5599/5596.cpp
@@ -16,11 +16,11 @@ using namespace std;
 
 int main() {
   int t, a = 0, b = 0;
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 4; i++) {
     cin >> t;
     a += t;
   }
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 4; i++) {
     cin >> t;
     b += t;
   }
