a9c1c62740b20eb5d9b7c1c28127f0c7658a7a39
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2023-01-08 01:33:43 +0900

    feat: (c++) 23222 Unique Shapes

---
+++ 23200~23299/23222.cpp
@@ -0,0 +1,20 @@
+// 23222: Unique Shapes
+//
+// - 문제
+// Imagine you have a lot of square blocks, and you can put them together into different shapes. You can make all kinds of shapes – but how many can you make that are truly unique?
+// We will consider a shape to be a connected group of blocks placed on a regular grid, where each block is connected to another block above it, below it, to its left, or to its right. Two shapes are considered unique if there is no sequence of rotations, translations, or reflections that can cause them to be identical.
+// With one block, only one unique shape can be made. With two blocks, the same is true. With three blocks, two unique shapes are possible, and with four blocks, five unique shapes are possible. The figure below gives illustrations of these cases:
+//
+// - 입력
+// There is no input for this problem.
+//
+// - 출력
+// Print out the number of unique shapes that can be constructed using 8 square blocks, as a decimal integer.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << 369;
+  return 0;
+}
