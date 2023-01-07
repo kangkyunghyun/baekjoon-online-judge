1fd2f5d2a16cdc69b35137053370ea7a78fd81da
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2023-01-08 00:05:55 +0900

    feat: (c++) 2667 단지번호붙이기

---
+++ 2600~2699/2667.cpp
@@ -0,0 +1,65 @@
+// 2667: 단지번호붙이기
+//
+// - 문제
+// <그림 1>과 같이 정사각형 모양의 지도가 있다. 1은 집이 있는 곳을, 0은 집이 없는 곳을 나타낸다. 철수는 이 지도를 가지고 연결된 집의 모임인 단지를 정의하고, 단지에 번호를 붙이려 한다. 여기서 연결되었다는 것은 어떤 집이 좌우, 혹은 아래위로 다른 집이 있는 경우를 말한다. 대각선상에 집이 있는 경우는 연결된 것이 아니다. <그림 2>는 <그림 1>을 단지별로 번호를 붙인 것이다. 지도를 입력하여 단지수를 출력하고, 각 단지에 속하는 집의 수를 오름차순으로 정렬하여 출력하는 프로그램을 작성하시오.
+//
+// - 입력
+// 첫 번째 줄에는 지도의 크기 N(정사각형이므로 가로와 세로의 크기는 같으며 5≤N≤25)이 입력되고, 그 다음 N줄에는 각각 N개의 자료(0혹은 1)가 입력된다.
+//
+// - 출력
+// 첫 번째 줄에는 총 단지수를 출력하시오. 그리고 각 단지내 집의 수를 오름차순으로 정렬하여 한 줄에 하나씩 출력하시오.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, cnt = 1;
+vector<string> graph(26);
+vector<vector<int>> visited(26, vector<int>(26, 0));
+int dy[] = {-1, 1, 0, 0};
+int dx[] = {0, 0, -1, 1};
+
+void bfs(int xx, int yy) {
+  queue<pair<int, int>> q;
+  q.push({xx, yy});
+  while (!q.empty()) {
+    int x = q.front().first;
+    int y = q.front().second;
+    visited[x][y] = cnt;
+    q.pop();
+    for (int i = 0; i < 4; i++) {
+      int nx = x + dx[i];
+      int ny = y + dy[i];
+      if (nx > 0 && ny > 0 && nx <= n && ny <= n && (graph[nx][ny - 1] - '0') && !visited[nx][ny]) {
+        q.push({nx, ny});
+        visited[nx][ny] = cnt;
+      }
+    }
+  }
+  cnt++;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL), cout.tie(NULL);
+  cin >> n;
+  for (int i = 1; i <= n; i++)
+    cin >> graph[i];
+  for (int i = 1; i <= n; i++)
+    for (int j = 1; j <= n; j++)
+      if ((graph[i][j - 1] - '0') && !visited[i][j])
+        bfs(i, j);
+  vector<int> ans;
+  for (int i = 1; i < cnt; i++) {
+    int tmp = 0;
+    for (int j = 1; j <= n; j++)
+      for (int k = 1; k <= n; k++)
+        if (visited[j][k] == i)
+          tmp++;
+    ans.push_back(tmp);
+  }
+  sort(ans.begin(), ans.end());
+  cout << cnt - 1 << '\n';
+  for (int i : ans)
+    cout << i << '\n';
+  return 0;
+}
