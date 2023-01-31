e91629997f9f3d809787394c3bc4e51bb7d3e6a4
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2023-01-31 18:01:45 +0900

    feat: (c++) 15650 N과 M (2)

---
+++ 15600~15699/15650.cpp
@@ -0,0 +1,44 @@
+// 15650: N과 M (2)
+//
+// - 문제
+// 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+// 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+// 고른 수열은 오름차순이어야 한다.
+//
+// - 입력
+// 첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
+//
+// - 출력
+// 한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
+// 수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> v(8, 0), visited(9, 0);
+
+void dfs(int cnt) {
+  if (cnt == m) {
+    for (int i = 0; i < m; i++)
+      cout << v[i] << ' ';
+    cout << '\n';
+    return;
+  }
+  for (int i = 1; i <= n; i++) {
+    if (!visited[i] && i > v[cnt - 1]) {
+      visited[i] = 1;
+      v[cnt] = i;
+      dfs(cnt + 1);
+      visited[i] = 0;
+    }
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL), cout.tie(NULL);
+  cin >> n >> m;
+  dfs(0);
+  return 0;
+}
