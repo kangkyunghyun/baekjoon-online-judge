f778db411fc21de7f0cbe94f29affc82be260a65
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2023-02-02 01:49:28 +0900

    fix: 개선

--- 9200~9299/9251.cpp
+++ 9200~9299/9251.cpp
@@ -17,7 +17,6 @@ int main() {
   string a, b;
   cin >> a >> b;
   vector<vector<int>> graph(a.size() + 1, vector<int>(b.size() + 1, 0));
-  int ans = 0;
   for (int i = 1; i <= a.size(); i++)
     for (int j = 1; j <= b.size(); j++)
       if (a[i - 1] == b[j - 1])
