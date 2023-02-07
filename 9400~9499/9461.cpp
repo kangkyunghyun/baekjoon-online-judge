75b5663fda42ceb12caf45127c9ac834ab8d646d
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2023-02-08 02:19:57 +0900

    fix: 내 생각대로 개선

--- 9400~9499/9461.cpp
+++ 9400~9499/9461.cpp
@@ -16,16 +16,17 @@ using namespace std;
 
 int main() {
   ios::sync_with_stdio(false);
-  cin.tie(NULL), cout.tie(NULL);
+  cin.tie(NULL);
   int t;
   cin >> t;
   while (t--) {
     int n;
     cin >> n;
-    vector<long long> v{1, 1, 1, 2, 2};
-    for (int i = 0, j = 4; v.size() < n; i++, j++)
-      v.push_back(v[i] + v[j]);
-    cout << v[n - 1] << '\n';
+    vector<long long> dp(n, 0);
+    dp[0] = 1, dp[1] = 1, dp[2] = 1;
+    for (int i = 3; i < n; i++)
+      dp[i] = dp[i - 2] + dp[i - 3];
+    cout << dp[n - 1] << '\n';
   }
   return 0;
 }
