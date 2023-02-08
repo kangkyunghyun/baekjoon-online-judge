ff6a3228d32f2f3f4330c7d5b69e4013f18427b8
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2023-02-08 17:11:31 +0900

    fix: int -> long long

--- 1000~1099/1041.cpp
+++ 1000~1099/1041.cpp
@@ -25,21 +25,20 @@ using namespace std;
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(NULL);
-  int n, a, b, c, d, e, f;
-  long long ans = 0;
+  long long n, a, b, c, d, e, f, ans = 0;
   cin >> n >> a >> b >> c >> d >> e >> f;
   if (n == 1) {
-    vector<int> v{a, b, c, d, e, f};
+    vector<long long> v{a, b, c, d, e, f};
     sort(v.begin(), v.end());
     for (int i = 0; i < 5; i++)
       ans += v[i];
   } else {
-    vector<int> v{a, b, c, d, e, f};
-    int one_face = *min_element(v.begin(), v.end());
+    vector<long long> v{a, b, c, d, e, f};
+    long long one_face = *min_element(v.begin(), v.end());
     v = {a + b, a + c, a + d, a + e, b + c, b + d, b + f, c + e, c + f, d + e, d + f, e + f};
-    int two_face = *min_element(v.begin(), v.end());
+    long long two_face = *min_element(v.begin(), v.end());
     v = {a + b + c, a + b + d, a + d + e, a + c + e, b + c + f, b + d + f, c + e + f, d + e + f};
-    int three_face = *min_element(v.begin(), v.end());
+    long long three_face = *min_element(v.begin(), v.end());
     ans += three_face * 4 + two_face * ((n - 2) * 4 + (n - 1) * 4) + one_face * ((n - 2) * (n - 2) + (n - 2) * (n - 1) * 4);
   }
   cout << ans << '\n';
