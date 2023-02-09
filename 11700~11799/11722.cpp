4dc49dc00b4a55b878ae3d1c52ee9b9f4f367829
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2023-02-09 13:37:53 +0900

    feat: (c++) 11722 가장 긴 감소하는 부분 수열

---
+++ 11700~11799/11722.cpp
@@ -0,0 +1,31 @@
+// 11722: 가장 긴 감소하는 부분 수열
+//
+// - 문제
+// 수열 A가 주어졌을 때, 가장 긴 감소하는 부분 수열을 구하는 프로그램을 작성하시오.
+// 예를 들어, 수열 A = {10, 30, 10, 20, 20, 10} 인 경우에 가장 긴 감소하는 부분 수열은 A = {10, 30, 10, 20, 20, 10}  이고, 길이는 3이다.
+//
+// - 입력
+// 첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000)이 주어진다.
+// 둘째 줄에는 수열 A를 이루고 있는 Ai가 주어진다. (1 ≤ Ai ≤ 1,000)
+//
+// - 출력
+// 첫째 줄에 수열 A의 가장 긴 감소하는 부분 수열의 길이를 출력한다.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  int n;
+  cin >> n;
+  vector<int> v(n), dp(n, 1);
+  for (int i = 0; i < n; i++)
+    cin >> v[i];
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < i; j++)
+      if (v[j] > v[i])
+        dp[i] = max(dp[i], dp[j] + 1);
+  cout << *max_element(dp.begin(), dp.end()) << '\n';
+  return 0;
+}
