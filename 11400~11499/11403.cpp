afb67027f99bb8d4ad80d5ecc38f6cdf0f644a0d
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2023-02-25 02:29:29 +0900

    fix: 주석 삭제

--- 11400~11499/11403.cpp
+++ 11400~11499/11403.cpp
@@ -18,19 +18,16 @@ int main() {
   cin.tie(NULL);
   int N, dist[101][101];
   cin >> N;
-  // dist 배열 초기화
   for (int i = 1; i <= N; i++)
     for (int j = 1; j <= N; j++) {
       cin >> dist[i][j];
       if (dist[i][j] == 0)
         dist[i][j] = INF;
     }
-  // 플로이드 워셜
   for (int k = 1; k <= N; k++)
     for (int i = 1; i <= N; i++)
       for (int j = 1; j <= N; j++)
         dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-  // 실제 최단 경로
   for (int i = 1; i <= N; i++) {
     for (int j = 1; j <= N; j++)
       cout << (dist[i][j] == INF ? 0 : 1) << ' ';
