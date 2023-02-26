e9e974dbf3853f2ba69170be6229327a2157566c
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2023-02-26 20:56:05 +0900

    fix: 개선

--- 27500~27599/27522.cpp
+++ 27500~27599/27522.cpp
@@ -29,16 +29,16 @@ int main() {
   cin.tie(NULL);
   vector<pair<string, char>> v;
   int arr[] = {10, 8, 6, 5, 4, 3, 2, 1};
-  string s;
-  while (cin >> s) {
+  for (int i = 0; i < 8; i++) {
+    string s;
     char t;
-    cin >> t;
+    cin >> s >> t;
     v.push_back({s, t});
   }
-  int R = 0, B = 0;
   sort(v.begin(), v.end());
+  int R = 0, B = 0;
   int size = v.size();
-  for (int i = 0; i < min(size, 8); i++)
+  for (int i = 0; i < 8; i++)
     if (v[i].second == 'R')
       R += arr[i];
     else
