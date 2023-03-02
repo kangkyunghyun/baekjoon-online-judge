5cd786b1570e37fc3ced0f3b38d086a297f25284
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2023-03-02 09:28:33 +0900

    fix: 개선

--- 4700~4799/4716.cpp
+++ 4700~4799/4716.cpp
@@ -39,8 +39,7 @@ int main() {
     sort(v.begin(), v.end(), cmp);
     for (int i = 0; i < N; i++) {
       int K = v[i].K, a = v[i].a, b = v[i].b;
-      int m = a > b ? 0 : 1;
-      if (m) {
+      if (a < b) {
         if (A >= K) {
           ans += K * a;
           A -= K;
