4359c648d78d973923a3684567bb67c2656f7821
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2023-04-10 00:20:18 +0900

    fix: var name

--- 27900~27999/27939.cpp
+++ 27900~27999/27939.cpp
@@ -24,7 +24,7 @@ using namespace std;
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  int n, a = 0;
+  int n, w = 0;
   cin >> n;
   vector<char> v(n);
   for (int i = 0; i < n; i++)
@@ -32,17 +32,17 @@ int main() {
   int m, k;
   cin >> m >> k;
   for (int i = 0; i < m; i++) {
-    int tmp = 0;
+    int p = 0;
     for (int j = 0; j < k; j++) {
       int x;
       cin >> x;
       if (v[x - 1] == 'P')
-        tmp++;
+        p++;
     }
-    if (tmp == 0)
-      a++;
+    if (p == 0)
+      w++;
   }
-  if (a != 0)
+  if (w != 0)
     cout << 'W';
   else
     cout << 'P';
