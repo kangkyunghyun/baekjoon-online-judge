bce7ab90728df2575cdfcfaa50d3a60af95c1768
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2023-04-14 02:49:09 +0900

    fix: 숏코딩 코드 추가

--- 2000~2099/2041.cpp
+++ 2000~2099/2041.cpp
@@ -33,11 +33,16 @@ int main() {
     b -= m / 2;
     if (m % 2 == 0) {
       if (i % 2 == 0)
-        b += 1;
+        b++;
       else
-        a -= 1;
+        a--;
     }
     cout << '\n';
   }
   return 0;
 }
+
+/* 숏코딩
+#include<iostream>
+int main(){int n,m;std::cin>>n>>m;int a=1,b=2*n*m-n-m+1,i=0,j;for(;i<n;i++){for(j=0;j<m;j++)std::cout<<((i+j)%2?b--:a++)<<' ';a+=m/2;b-=m/2;if(m%2==0)i%2?a--:b++;std::cout<<'\n';}}
+*/
