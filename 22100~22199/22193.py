c9ce7c31359dd95f1755bb0135f8bc9ff48c8261
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2023-06-08 03:01:09 +0900

    feat: (python) 22193 Multiply

---
+++ 22100~22199/22193.py
@@ -0,0 +1,20 @@
+# 22193: Multiply
+#
+# - 문제
+# Write a program that computes a product of two non-negative integers A and B.
+# The integers are represented in decimal notation and have N and M digits, respectively.
+#
+# - 입력
+# The first line contains the lengths N and M, separated by a space.
+# A is given on the second and B on the third line. The numbers will not have leading zeros.
+#
+# - 출력
+# Output the product of A and B without leading zeros.
+#
+# - 제한
+# 1 ≤ N, M ≤ 50 000
+
+a, b = input().split()
+c = int(input())
+d = int(input())
+print(c*d)
