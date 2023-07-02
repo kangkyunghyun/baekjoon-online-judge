ee556e5ba2e9dd8704789a2ecc5efcef12daba93
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2023-07-03 02:23:25 +0900

    feat: (c++) 10189 Hook

---
+++ 10100~10199/10189.cpp
@@ -0,0 +1,17 @@
+// 10189: Hook
+//
+// - 문제
+// Print out the word Hook as shown below.
+//
+// - 출력
+// Print out the word Hook as shown below.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout << "#  # #### #### #  #\n#### #  # #  # # #\n#### #  # #  # # #\n#  # #### #### #  #";
+  return 0;
+}
