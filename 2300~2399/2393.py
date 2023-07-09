2a13a277d5e9ec012453433f4ec8ac411ede05c2
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2023-07-10 04:48:33 +0900

    feat: (python) 2393 Rook

---
+++ 2300~2399/2393.py
@@ -0,0 +1,22 @@
+# 2393: Rook
+#
+# - 문제
+# You have just learned how to output text to the screen and your teacher has challenged you to create an ASCII art of a chess piece. You have decided to make your favorite piece, the rook.
+#
+# - 출력
+# The rook art, exactly as shown below, with no extra blank spaces. In particular, a line must not end with a blank space.
+
+a = '''  ___  ___  ___
+  | |__| |__| |
+  |           |
+   \_________/
+    \_______/
+     |     |
+     |     |
+     |     |
+     |     |
+     |_____|
+  __/       \__
+ /             \\
+/_______________\\'''
+print(a)
