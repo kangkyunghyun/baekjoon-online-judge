48100ed2a0e55ff180dc8de5c35ba27c7fa5b9b9
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2023-09-07 19:06:20 +0900

    feat: (c++) 2957 이진 탐색 트리

---
+++ 2900~2999/2957.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n;
+  ll ans = 0;
+  cin >> n;
+  map<int, ll> m;
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    if (m.empty()) {
+      m[x] = 0;
+    } else {
+      auto it = m.lower_bound(x);
+      ll tmp = 0;
+      if (it != m.end())
+        tmp = max(tmp, it->second);
+      if (it != m.begin()) {
+        it--;
+        tmp = max(tmp, it->second);
+      }
+      ans += tmp + 1;
+      m[x] = tmp + 1;
+    }
+    cout << ans << '\n';
+  }
+  return 0;
+}
