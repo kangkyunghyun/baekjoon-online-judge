3e4b749f09f1db1997ca086fa0e654ba7d1f7615
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2023-09-18 03:56:37 +0900

    fix: (c++) 30021 순열 선물하기

--- code/30000~30099/30021.cpp
+++ code/30000~30099/30021.cpp
@@ -1,41 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-bool flag = 0;
-vector<int> v(5001), visited(5001, 0);
-
-bool isPrime(int n) {
-  if (n < 2)
-    return false;
-  for (int i = 2; i * i <= n; i++)
-    if (!(n % i))
-      return false;
-  return true;
-}
-
-void dfs(int cnt, int sum) {
-  if (cnt == n) {
-    cout << "YES\n";
-    for (int i = 0; i < n; i++)
-      cout << v[i] << ' ';
-    flag = 1;
-    return;
-  }
-  for (int i = 1; !flag && i <= n; i++) {
-    if (!visited[i] && !isPrime(sum + i)) {
-      visited[i] = 1;
-      v[cnt] = i;
-      dfs(cnt + 1, sum + i);
-      visited[i] = 0;
-    }
-  }
-}
-
 int main() {
   cin.tie(0)->sync_with_stdio(0);
+  int n;
   cin >> n;
-  dfs(0, 0);
-  if (!flag)
+  if (n == 2)
     cout << "NO";
+  else if (n == 1)
+    cout << "YES\n1";
+  else {
+    cout << "YES\n1 3 2 ";
+    for (int i = 4; i <= n; i++)
+      cout << i << ' ';
+  }
 }
