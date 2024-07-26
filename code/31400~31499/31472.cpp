49d8b86ee3a0ef331792716fb155196ea1aa7799
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2024-07-26 14:39:30 +0000

    feat: (c++) 31472 갈래의 색종이 자르기

---
+++ code/31400~31499/31472.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int w;
+    cin>>w;
+    double ans=4*sqrt(2*w);
+    cout<<ans;
+}
