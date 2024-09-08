38ae969cdbdd4a055c9b0d29627b4a6e16e6d80a
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2024-09-08 14:58:40 +0900

    feat: (c++) 32215 코드마스터 2024

---
+++ code/32200~32299/32215.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n, m, k;
+    cin >> n >> m >> k;
+    cout << (k + 1) * m;
+}
