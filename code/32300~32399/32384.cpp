1938eddcbded2eb2a65e0179c833a0643724fe9f
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-04-03 18:55:44 +0900

    feat: (c++) 32384 사랑은 고려대입니다

---
+++ code/32300~32399/32384.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n;
+    cin >> n;
+    string s = "LoveisKoreaUniversity";
+    while (--n)
+        cout << s << ' ';
+    cout << s;
+}
