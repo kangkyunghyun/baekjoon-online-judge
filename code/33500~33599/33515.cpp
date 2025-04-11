4a3486de09c13b2f11f1c988399b3c4e76e09033
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-04-11 18:36:05 +0900

    feat: (c++) 33515 노트북 세 대를 가지고 왔다

---
+++ code/33500~33599/33515.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int a, b;
+    cin >> a >> b;
+    cout << min(a, b);
+}
