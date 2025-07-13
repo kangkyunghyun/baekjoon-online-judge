b4c7dbed181874883cfe9a387c1b2f2bc571184c
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-07-14 02:38:16 +0900

    fix: power function - recursion to loop

--- algorithm/pow.cpp
+++ algorithm/pow.cpp
@@ -1,9 +1,11 @@
 // pow O(log N)
-int pow(int a, int b) {
-    if (b == 1)
-        return a;
-    if (b % 2)
-        return a * pow(a * a, (b - 1) / 2);
-    else
-        return pow(a * a, b / 2);
+int pow(int n, int k, int mod) {
+    int ret = 1, base = n % mod;
+    while (k) {
+        if (k % 2)
+            ret = (ret * base) % mod;
+        base = (base * base) % mod;
+        k /= 2;
+    }
+    return ret;
 }
