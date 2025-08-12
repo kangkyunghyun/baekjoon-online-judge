60604d2241173360dcbaa972f9068050371fbd27
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-08-13 01:04:10 +0900

    feat: Shoelace Formula

---
+++ algorithm/Shoelace-Formula.cpp
@@ -0,0 +1,21 @@
+// Shoelace-Formula
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n;
+    cin >> n;
+    vector<pair<int, int>> v(n);
+    for (auto &[i, j] : v)
+        cin >> i >> j;
+    v.push_back(v[0]);
+    int a = 0, b = 0;
+    for (int i = 1; i <= n; i++) {
+        a += v[i - 1].first * v[i].second;
+        b += v[i - 1].second * v[i].first;
+    }
+    cout.precision(1);
+    cout << fixed << (double)abs(a - b) / 2;
+}
