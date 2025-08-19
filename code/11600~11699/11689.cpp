a8fc69128cc210d474b5606d9e5b5d37a7b7d3e1
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-08-20 02:22:37 +0900

    feat: (c++) 11689 GCD(n, k) = 1

---
+++ code/11600~11699/11689.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n;
+    cin >> n;
+    int ans = n;
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0) {
+            ans -= ans / i;
+            while (n % i == 0)
+                n /= i;
+        }
+    if (n > 1)
+        ans -= ans / n;
+    cout << ans;
+}
