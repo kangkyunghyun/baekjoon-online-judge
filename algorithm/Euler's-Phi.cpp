3641964a70e70499db8b6d2d9bacbc9745f52035
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-08-20 02:34:53 +0900

    fix: 함수화

--- algorithm/Euler's-Phi.cpp
+++ algorithm/Euler's-Phi.cpp
@@ -3,18 +3,22 @@
 using namespace std;
 #define int long long
 
+int eulerPhi(int num) {
+    int ret = num;
+    for (int i = 2; i * i <= num; i++)
+        if (num % i == 0) {
+            ret -= ret / i;
+            while (num % i == 0)
+                num /= i;
+        }
+    if (num > 1)
+        ret -= ret / num;
+    return ret;
+}
+
 signed main() {
     cin.tie(0)->sync_with_stdio(0);
     int n;
     cin >> n;
-    int ans = n;
-    for (int i = 2; i * i <= n; i++)
-        if (n % i == 0) {
-            ans -= ans / i;
-            while (n % i == 0)
-                n /= i;
-        }
-    if (n > 1)
-        ans -= ans / n;
-    cout << ans;
+    cout << eulerPhi(n);
 }
