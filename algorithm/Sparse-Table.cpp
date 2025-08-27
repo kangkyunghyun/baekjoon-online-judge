81f0f0cfdb725a772cf010bca376a3c03932a703
Kyunghyun Kang <kangkh0906@khu.ac.kr>
2025-08-28 02:07:11 +0900

    feat: Sparse-Table Algorithm

---
+++ algorithm/Sparse-Table.cpp
@@ -0,0 +1,27 @@
+// Sparse-Table O(NlogN)
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define MAX 19
+
+int m, q, n, x, table[500001][MAX];
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin >> m;
+    for (int i = 1; i <= m; i++)
+        cin >> table[i][0];
+    for (int j = 1; j < MAX; j++)
+        for (int i = 1; i <= m; i++)
+            table[i][j] = table[table[i][j - 1]][j - 1];
+    cin >> q;
+    while (q--) {
+        cin >> n >> x;
+        for (int i = MAX - 1; i >= 0; i--)
+            if (n >= 1 << i) {
+                n -= 1 << i;
+                x = table[x][i];
+            }
+        cout << x << '\n';
+    }
+}
