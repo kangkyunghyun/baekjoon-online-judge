9d228995380a07f65efe7548e7538556fd49912a
강경현 <kangkh0906@gmail.com>
2026-03-22 02:24:41 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    for (int j = n; j > n - i; j--) {
+      cout << ' ';
+    }
+    for (int k = n; k > i; k--) {
+      cout << '*';
+    }
+    cout << '\n';
+  }
+  return 0;
+}
