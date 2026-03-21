f18c19a7b672f52cbc49a44abdd1da80e12e0294
강경현 <kangkh0906@gmail.com>
2026-03-22 03:24:30 +0900

    [Silver V] Title: 알록달록 앵무새, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/28445. 알록달록 앵무새/알록달록 앵무새.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  set<string> s;
+  for (int i = 0; i < 4; i++) {
+    string tmp;
+    cin >> tmp;
+    s.insert(tmp);
+  }
+  for (auto i = s.begin(); i != s.end(); i++)
+    for (auto j = s.begin(); j != s.end(); j++)
+      cout << *i << ' ' << *j << '\n';
+  return 0;
+}
