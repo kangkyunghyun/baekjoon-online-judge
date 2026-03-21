5873bdfcebca14f70a0928b63bc65e267f7d056c
강경현 <kangkh0906@gmail.com>
2026-03-22 04:04:35 +0900

    [Silver I] Title: 우리집엔 도서관이 있어, Time: 32 ms, Memory: 4364 KB -BaekjoonHub

--- 백준/Silver/2872. 우리집엔 도서관이 있어/우리집엔 도서관이 있어.cc
+++ 백준/Silver/2872. 우리집엔 도서관이 있어/우리집엔 도서관이 있어.cc
@@ -6,14 +6,11 @@ signed main() {
     cin.tie(0)->sync_with_stdio(0);
     int n;
     cin >> n;
-    int ans = n;
     vector<int> v(n);
     for (int i = 0; i < n; ++i)
         cin >> v[i];
     for (int i = n - 1; i >= 0; --i)
-        if (v[i] == n) {
+        if (v[i] == n)
             n--;
-            ans--;
-        }
-    cout << ans;
+    cout << n;
 }
