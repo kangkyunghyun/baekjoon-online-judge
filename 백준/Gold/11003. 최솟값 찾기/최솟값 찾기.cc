8ba0b4a342d3ae369bb872d1807d5de0ca3dc09d
강경현 <kangkh0906@gmail.com>
2026-03-22 03:05:46 +0900

    [Gold I] Title: 최솟값 찾기, Time: 1776 ms, Memory: 100452 KB -BaekjoonHub

--- 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.cc
+++ 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -1,18 +1,3 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(NULL);
-  int n, l, x;
-  cin >> n >> l;
-  priority_queue<pair<int, int>> pq;
-  for (int i = 1; i <= n; i++) {
-    cin >> x;
-    pq.push({-x, i});
-    while (pq.top().second <= i - l)
-      pq.pop();
-    cout << -pq.top().first << ' ';
-  }
-  return 0;
-}
+int main(){ios::sync_with_stdio(0);cin.tie(0);int n,l,x;cin>>n>>l;priority_queue<pair<int,int>>q;for(int i=1;i<=n;i++){cin>>x;q.push({-x,i});while(q.top().second<=i-l)q.pop();cout<<-q.top().first<<' ';}}
