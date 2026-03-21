4d3837790c1e4865ab4d63147cd039852b648025
강경현 <kangkh0906@gmail.com>
2026-03-22 02:21:07 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,9 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b, c, d, e;
+  cin >> a >> b >> c >> d >> e;
+  cout << int(pow(a, 2) + pow(b, 2) + pow(c, 2) + pow(d, 2) + pow(e, 2)) % 10 << endl;
+}
