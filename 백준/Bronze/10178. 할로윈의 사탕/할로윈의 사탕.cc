dbee63905dcc732dbae7104d3f2285f176e92d8d
강경현 <kangkh0906@gmail.com>
2026-03-22 01:56:55 +0900

    [Bronze III] Title: 할로윈의 사탕, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t, c, v;
+  cin >> t;
+  for (int i = 0; i < t; i++) {
+    cin >> c >> v;
+    cout << "You get " << c / v << " piece(s) and your dad gets " << c % v << " piece(s)." << '\n';
+  }
+  return 0;
+}
