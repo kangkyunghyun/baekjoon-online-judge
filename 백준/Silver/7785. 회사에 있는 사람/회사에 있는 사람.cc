09ebf1db3af9116d07998b92648e64afd4b1e939
강경현 <kangkh0906@gmail.com>
2026-03-22 01:59:11 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 164 ms, Memory: 16000 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  map<string, bool> m;
+  for (int i = 0; i < n; i++) {
+    string name, check;
+    cin >> name >> check;
+    if (check == "enter")
+      m[name] = true;
+    else if (check == "leave")
+      m[name] = false;
+  }
+  vector<string> v;
+  for (auto i : m)
+    if (i.second)
+      v.push_back(i.first);
+  for (int i = v.size() - 1; i >= 0; i--)
+    cout << v[i] << '\n';
+  return 0;
+}
