448edf47836bde3d87abbb47c977eb2636a5af70
강경현 <kangkh0906@gmail.com>
2026-03-22 02:35:50 +0900

    [Bronze I] Title: N번째 큰 수, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.cc
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  int arr[10] = {0};
+  for (int i = 0; i < t; i++) {
+    for (int j = 0; j < 10; j++)
+      cin >> arr[j];
+    sort(arr, arr + 10);
+    cout << arr[7] << '\n';
+  }
+  return 0;
+}
