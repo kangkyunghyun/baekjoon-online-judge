b734648473e737dc16611511dcb09e5f7ef141d5
강경현 <kangkh0906@gmail.com>
2026-03-22 01:57:13 +0900

    [Bronze II] Title: 소수 찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, a, result = 0;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> a;
+    int cnt = 0;
+    for (int j = 1; j < a; j++) {
+      if (a % j == 0)
+        cnt++;
+    }
+    if (cnt == 1)
+      result++;
+  }
+  cout << result << '\n';
+  return 0;
+}
