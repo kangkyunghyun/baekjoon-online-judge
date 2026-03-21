f1670081cfdd7943a3ac48e436ced4edf7f5617e
강경현 <kangkh0906@gmail.com>
2026-03-22 02:22:19 +0900

    [Bronze V] Title: Football Scoring, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int T, F, S, P, C;
+  cin >> T >> F >> S >> P >> C;
+  cout << T * 6 + F * 3 + S * 2 + P + C * 2 << ' ';
+  cin >> T >> F >> S >> P >> C;
+  cout << T * 6 + F * 3 + S * 2 + P + C * 2 << '\n';
+  return 0;
+}
