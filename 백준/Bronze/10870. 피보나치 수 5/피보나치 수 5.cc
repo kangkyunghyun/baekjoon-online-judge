b0eb9294a45aecf3b0186fcda65b6594aa06c041
강경현 <kangkh0906@gmail.com>
2026-03-22 01:57:30 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -2,13 +2,12 @@
 using namespace std;
 
 int fibonacci(int n) {
-  if (n == 0) {
+  if (n == 0)
     return 0;
-  } else if (n == 1) {
+  else if (n == 1)
     return 1;
-  } else {
+  else
     return fibonacci(n - 1) + fibonacci(n - 2);
-  }
 }
 int main() {
   int n;
