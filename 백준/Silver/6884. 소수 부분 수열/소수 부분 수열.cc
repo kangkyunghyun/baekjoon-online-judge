9642c17b961eba5064c0a1d38505cfb0ee64aaad
강경현 <kangkh0906@gmail.com>
2026-03-22 02:01:11 +0900

    [Silver II] Title: 소수 부분 수열, Time: 104 ms, Memory: 2308 KB -BaekjoonHub

---
+++ 백준/Silver/6884. 소수 부분 수열/소수 부분 수열.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int x) {
+  int i = 2;
+  while (i * i <= x) {
+    if (x % i == 0)
+      return false;
+    i++;
+  }
+  return true;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL), cout.tie(NULL);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, tmp = 0;
+    cin >> n;
+    vector<int> v(n), sum;
+    sum.push_back(0);
+    for (int i = 0; i < n; i++) {
+      cin >> v[i];
+      tmp += v[i];
+      sum.push_back(tmp);
+    }
+    int size = 2, flag = 0;
+    while (size <= n) {
+      for (int i = 1, j = i + size - 1; j <= n; i++, j++) {
+        int s = sum[j] - sum[i - 1];
+        if (isPrime(s)) {
+          cout << "Shortest primed subsequence is length " << size << ": ";
+          for (int k = i - 1; k < j; k++)
+            cout << v[k] << ' ';
+          cout << '\n';
+          flag = 1;
+          break;
+        }
+      }
+      if (flag)
+        break;
+      size++;
+    }
+    if (!flag)
+      cout << "This sequence is anti-primed.\n";
+  }
+  return 0;
+}
