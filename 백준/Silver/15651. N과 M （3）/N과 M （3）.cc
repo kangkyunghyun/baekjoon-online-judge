6c4067d95bc0b9fc440576f2f4e853d5e3f338bb
강경현 <kangkh0906@gmail.com>
2026-03-22 02:59:52 +0900

    [Silver III] Title: N과 M (3), Time: 284 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> v(7, 0);
+
+void dfs(int cnt) {
+  if (cnt == m) {
+    for (int i = 0; i < m; i++)
+      cout << v[i] << ' ';
+    cout << '\n';
+    return;
+  }
+  for (int i = 1; i <= n; i++) {
+    v[cnt] = i;
+    dfs(cnt + 1);
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL), cout.tie(NULL);
+  cin >> n >> m;
+  dfs(0);
+  return 0;
+}
