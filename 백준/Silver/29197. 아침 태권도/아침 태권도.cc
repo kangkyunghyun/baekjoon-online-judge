1dabe20f0d0214a857a4bacda32353d652ae86ca
강경현 <kangkh0906@gmail.com>
2026-03-22 03:25:06 +0900

    [Silver I] Title: 아침 태권도, Time: 0 ms, Memory: 2156 KB, Score: 10 point -BaekjoonHub

--- 백준/Silver/29197. 아침 태권도/아침 태권도.cc
+++ 백준/Silver/29197. 아침 태권도/아침 태권도.cc
@@ -6,25 +6,24 @@ int main() {
   cin.tie(0);
   int n;
   cin >> n;
-  vector<pair<int, int>> v(n);
-  for (int i = 0; i < n; i++)
-    cin >> v[i].first >> v[i].second;
   set<pair<double, int>> s;
-  for (auto [x, y] : v) {
+  for (int i = 0; i < n; i++) {
+    double x, y;
+    cin >> x >> y;
     double t = 0;
     int a;
     if (x > 0 && y > 0) {
       a = 1;
-      t = (double)y / x;
+      t = y / x;
     } else if (x < 0 && y > 0) {
       a = 2;
-      t = (double)y / x;
+      t = y / x;
     } else if (x < 0 && y < 0) {
       a = 3;
-      t = (double)y / x;
+      t = y / x;
     } else if (x > 0 && y < 0) {
       a = 4;
-      t = (double)y / x;
+      t = y / x;
     } else if (x == 0 && y > 0)
       a = 5; // y축 위
     else if (x == 0 && y < 0)
