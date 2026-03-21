07007a4c79781f37df45de78891c341eb0c4d202
강경현 <kangkh0906@gmail.com>
2026-03-22 02:46:53 +0900

    [Silver I] Title: IOIOI, Time: 8 ms, Memory: 3680 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -5,16 +5,19 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(NULL), cout.tie(NULL);
   int n, m, ans = 0;
-  cin >> n >> m;
-  string IOI = "I", s;
-  while (n--)
-    IOI += "OI";
-  cin >> s;
-  int index = 0, findIndex = s.find(IOI);
-  while (findIndex != string::npos) {
-    ans++;
-    index = findIndex + 1;
-    findIndex = s.find(IOI, index);
+  string s;
+  cin >> n >> m >> s;
+  for (int i = 1; i < s.length(); i++) {
+    int cnt = 0;
+    if (s[i - 1] == 'I')
+      while (s[i] == 'O' && s[i + 1] == 'I') {
+        cnt++;
+        i += 2;
+        if (cnt == n) {
+          cnt--;
+          ans++;
+        }
+      }
   }
   cout << ans << '\n';
   return 0;
