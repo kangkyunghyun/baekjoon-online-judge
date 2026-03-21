95e9a4beaf14bff2eebc1cd4c3c620630e68cacd
강경현 <kangkh0906@gmail.com>
2026-03-22 01:53:59 +0900

    [Bronze V] Title: 크냐?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b;
+  while (cin >> a >> b) {
+    if (a == 0 && b == 0) {
+      break;
+    } else if (a > b) {
+      cout << "Yes" << '\n';
+    } else {
+      cout << "No" << '\n';
+    }
+  }
+}
