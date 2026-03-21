8b5d681cf90ddf9e26559e3f80a925c1ca687fb3
강경현 <kangkh0906@gmail.com>
2026-03-22 03:22:03 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  while (1) {
+    int a, b;
+    cin >> a >> b;
+    if (a + b == 0)
+      break;
+    if (a < b && b % a == 0)
+      cout << "factor\n";
+    else if (a > b && a % b == 0)
+      cout << "multiple\n";
+    else
+      cout << "neither\n";
+  }
+  return 0;
+}
