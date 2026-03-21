6d345f51a5683279ab90a2fdccdd6b2eb03ad4f5
강경현 <kangkh0906@gmail.com>
2026-03-22 03:00:16 +0900

    [Silver I] Title: 수학 퀴즈, Time: 40 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/27467. 수학 퀴즈/수학 퀴즈.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  int n;
+  double p = 0, q = 0;
+  cin >> n;
+  while (n--) {
+    int x;
+    cin >> x;
+    if (x > 3) {
+      x %= 3;
+    }
+    if (x == 1) {
+      p++;
+    } else if (x == 2) {
+      p--;
+      q--;
+    } else {
+      q++;
+    }
+  }
+  cout.precision(10);
+  cout << fixed << p << ' ' << q << '\n';
+  return 0;
+}
