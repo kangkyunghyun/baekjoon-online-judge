e95e947a2d77b0b19b8d2a21427f62e3b8d8f16a
강경현 <kangkh0906@gmail.com>
2026-03-22 03:40:58 +0900

    [Bronze V] Title: Metronome, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  double n;
+  cin >> n;
+  cout.precision(2);
+  cout << fixed << n / 4;
+  return 0;
+}
