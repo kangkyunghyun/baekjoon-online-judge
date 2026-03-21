6822bf33def56d7a13d8c4a30ac7632648e64779
강경현 <kangkh0906@gmail.com>
2026-03-22 02:22:59 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 44 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  string a;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> a;
+    if (a.length() >= 6 && a.length() <= 9) {
+      cout << "yes" << '\n';
+    } else {
+      cout << "no" << '\n';
+    }
+  }
+  return 0;
+}
