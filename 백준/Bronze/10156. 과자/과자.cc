0fce27573b378ab0bb61fc94d65c19b9729f9867
강경현 <kangkh0906@gmail.com>
2026-03-22 02:51:06 +0900

    [Bronze IV] Title: 과자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int k, n, m;
+  cin >> k >> n >> m;
+  if (k * n >= m)
+    cout << k * n - m;
+  else
+    cout << 0;
+  return 0;
+}
