c1c8c2b74feaad22fc020685d2e1d2dcf40de2cb
강경현 <kangkh0906@gmail.com>
2026-03-22 03:55:23 +0900

    [Bronze IV] Title: 관공... 어찌하여 목만 오셨소..., Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/30501. 관공．．． 어찌하여 목만 오셨소．．．/관공．．． 어찌하여 목만 오셨소．．．.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    string x;
+    cin >> n;
+    while(n--){
+        cin >> x;
+        if (x.find('S')!=-1)
+        {
+            cout<<x;
+            continue;
+        }
+    }
+}
