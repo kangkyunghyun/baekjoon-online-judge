a632ce17fc07f04ad62bea4c49a4dff92cbdbc97
강경현 <kangkh0906@gmail.com>
2026-03-22 04:52:23 +0900

    [Bronze III] Title: 다음수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4880. 다음수/다음수.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  while (1) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (!a && !b && !c)
+      break;
+    if (b - a == c - b)
+      cout << "AP " << c + c - b;
+    else
+      cout << "GP " << c * c / b;
+    cout << '\n';
+  }
+  return 0;
+}
