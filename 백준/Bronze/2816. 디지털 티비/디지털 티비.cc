76669082df4fc1d99ab4e53b6dda4b9fd260756b
강경현 <kangkh0906@gmail.com>
2026-03-22 02:54:10 +0900

    [Bronze I] Title: 디지털 티비, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2816. 디지털 티비/디지털 티비.cc
+++ 백준/Bronze/2816. 디지털 티비/디지털 티비.cc
@@ -1,6 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool check(int n) {
+  vector<int> v;
+  for (int i = 1; i <= n; i++)
+    if (n % i == 0)
+      v.push_back(i);
+  int sum = 0;
+  for (int i = 0; i < v.size() - 1; i++)
+    sum += v[i];
+  if (sum > n)
+    return true;
+  else
+    return false;
+}
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(NULL), cout.tie(NULL);
