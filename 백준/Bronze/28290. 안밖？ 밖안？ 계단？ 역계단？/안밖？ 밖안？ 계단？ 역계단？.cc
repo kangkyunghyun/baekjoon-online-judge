7552117560bb9744bd88eb1d458773e4465c94e3
강경현 <kangkh0906@gmail.com>
2026-03-22 03:40:15 +0900

    [Bronze IV] Title: 안밖? 밖안? 계단? 역계단?, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28290. 안밖？ 밖안？ 계단？ 역계단？/안밖？ 밖안？ 계단？ 역계단？.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  string s;
+  cin >> s;
+  if (s == "fdsajkl;" || s == "jkl;fdsa")
+    cout << "in-out";
+  else if (s == "asdf;lkj" || s == ";lkjasdf")
+    cout << "out-in";
+  else if (s == "asdfjkl;")
+    cout << "stairs";
+  else if (s == ";lkjfdsa")
+    cout << "reverse";
+  else
+    cout << "molu";
+  return 0;
+}
