7f29747b3e6c5a1786d814b46014fe7112cdfb23
강경현 <kangkh0906@gmail.com>
2026-03-22 03:19:56 +0900

    [Bronze IV] Title: 빵, Time: 0 ms, Memory: 2020 KB, Score: 16 point -BaekjoonHub

---
+++ 백준/Bronze/25377. 빵/빵.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n, m = 1001, a, b;
+  cin >> n;
+  while (n--) {
+    cin >> a >> b;
+    if (a < b)
+      m = min(m, b);
+  }
+  cout << (m > 1000 ? -1 : m);
+  return 0;
+}
