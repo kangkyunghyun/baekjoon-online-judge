f732ac68d7787bd06ccc289b3f11d3842959312e
강경현 <kangkh0906@gmail.com>
2026-03-22 03:42:47 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  vector<int> v(9);
+  for (int i = 0; i < 9; i++)
+    cin >> v[i];
+  int sum = accumulate(v.begin(), v.end(), 0);
+  for (int i = 0; i < 8; i++)
+    for (int j = i + 1; j < 9; j++)
+      if (sum - v[i] - v[j] == 100) {
+        vector<int> ans;
+        for (int k = 0; k < 9; k++) {
+          if (k == i || k == j)
+            continue;
+          ans.push_back(v[k]);
+        }
+        sort(ans.begin(), ans.end());
+        for (int e : ans)
+          cout << e << '\n';
+        return 0;
+      }
+  return 0;
+}
