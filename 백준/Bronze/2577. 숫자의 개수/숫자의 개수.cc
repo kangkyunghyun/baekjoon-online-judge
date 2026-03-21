f9447188fe26b8fe54b33fdad8867d62e3d2a2dc
강경현 <kangkh0906@gmail.com>
2026-03-22 01:55:49 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int a, b, c, cnt = 0;
+  string str;
+  int arr[10] = {0};
+  cin >> a >> b >> c;
+  str = to_string(a * b * c);
+  for (int i = 0; i < 10; i++) {
+    cnt = 0;
+    char k = char(i + 48);
+    for (int j = 0; j < str.length(); j++) {
+      if (k == str[j]) {
+        cnt++;
+      }
+    }
+    arr[i] = cnt;
+  }
+  for (int i = 0; i < 10; i++) {
+    cout << arr[i] << '\n';
+  }
+  return 0;
+}
