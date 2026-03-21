f9610a5dd5925100c9e099312979fafb56c55411
강경현 <kangkh0906@gmail.com>
2026-03-22 03:17:10 +0900

    [Silver V] Title: The primes contain arbitrarily long arithmetic progressions, Time: 0 ms, Memory: 2020 KB, Score: 7 point -BaekjoonHub

--- 백준/Silver/27907. The primes contain arbitrarily long arithmetic progressions/The primes contain arbitrarily long arithmetic progressions.cc
+++ 백준/Silver/27907. The primes contain arbitrarily long arithmetic progressions/The primes contain arbitrarily long arithmetic progressions.cc
@@ -4,9 +4,9 @@ using namespace std;
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  int n;
+  long long n;
   cin >> n;
-  for (int i = 0; i < n; i++)
-    cout << 2 << ' ';
+  for (long long i = 0; i < n; i++)
+    cout << 3 + 2 * i << ' ';
   return 0;
 }
