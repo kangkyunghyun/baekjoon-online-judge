c813b89770ff552c94d1d3f809b05fc42b0870bb
강경현 <kangkh0906@gmail.com>
2026-03-22 02:54:24 +0900

    [Silver IV] Title: 춤, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

--- 백준/Silver/4836. 춤/춤.cc
+++ 백준/Silver/4836. 춤/춤.cc
@@ -11,64 +11,63 @@ int main() {
     stringstream stream(s);
     while (stream >> sss)
       ss.push_back(sss);
-    vector<int> errors;
+    vector<int> errors(6, 0);
     int size = ss.size();
-    if (ss[0] == "jiggle")
-      errors.push_back(4);
-    if (size >= 3 && !(ss[size - 3] == "clap" && ss[size - 2] == "stomp" && ss[size - 1] == "clap"))
-      errors.push_back(2);
-    else if (size < 3)
-      errors.push_back(2);
     int twirl = 0, hop = 0, dip = 0;
-    for (int i = 0; i < ss.size(); i++) {
-      if (ss[i] == "twirl")
-        twirl = 1;
-      if (ss[i] == "hop")
-        hop = 1;
+    for (int i = 0; i < size; i++) {
       if (ss[i] == "dip") {
         dip = 1;
-        int flag = 1;
-        if (i - 1 >= 0 && ss[i - 1] == "jiggle")
-          flag = 0;
-        else if (i - 2 >= 0 && ss[i - 2] == "jiggle")
-          flag = 0;
-        else if (i + 1 < size && ss[i + 1] == "twirl")
-          flag = 0;
-        if (flag) {
-          errors.push_back(1);
+        if (!((i - 1 >= 0 && ss[i - 1] == "jiggle") || (i - 2 >= 0 && ss[i - 2] == "jiggle") || (i + 1 < size && ss[i + 1] == "twirl"))) {
+          errors[1] = 1;
           ss[i] = "DIP";
+        }
+      } else if (ss[i] == "twirl") {
+        twirl = 1;
+      } else if (ss[i] == "hop") {
+        hop = 1;
+      }
+    }
+    if (size < 3 || !(ss[size - 1] == "clap" && ss[size - 2] == "stomp" && ss[size - 3] == "clap"))
+      errors[2] = 1;
+    if (twirl && !hop)
+      errors[3] = 1;
+    if (ss[0] == "jiggle")
+      errors[4] = 1;
+    if (!dip)
+      errors[5] = 1;
+    int cnt = 0;
+    for (int i = 1; i <= 5; i++)
+      if (errors[i] == 1)
+        cnt++;
+    cout << "form ";
+    if (cnt == 0) {
+      cout << "ok: ";
+    } else if (cnt == 1) {
+      cout << "error ";
+      for (int i = 1; i <= 5; i++)
+        if (errors[i] == 1)
+          cout << i << ": ";
+    } else {
+      cout << "errors ";
+      int tmp = 0;
+      for (int i = 1; i <= 5; i++) {
+        if (errors[i] == 1) {
+          tmp++;
+          cout << i;
+          if (tmp == 0)
+            continue;
+          else if (tmp < cnt - 1)
+            cout << ", ";
+          else if (tmp == cnt - 1)
+            cout << " and ";
+          else
+            cout << ": ";
         }
       }
     }
-    if (!dip)
-      errors.push_back(5);
-    if (twirl && !hop)
-      errors.push_back(3);
-    errors.erase(unique(errors.begin(), errors.end()), errors.end());
-    sort(errors.begin(), errors.end());
-    int errSize = errors.size();
-    if (errSize == 0) {
-      cout << "form ok: " << s << '\n';
-    } else if (errSize == 1) {
-      cout << "form error " << errors[0] << ": ";
-      for (string i : ss)
-        cout << i << ' ';
-      cout << '\n';
-    } else if (errSize > 1) {
-      cout << "form errors ";
-      for (int i = 0; i < errSize; i++) {
-        cout << errors[i];
-        if (i == errSize - 2)
-          cout << " and ";
-        else if (i == errSize - 1)
-          cout << ": ";
-        else
-          cout << ", ";
-      }
-      for (string i : ss)
-        cout << i << ' ';
-      cout << '\n';
-    }
+    for (string i : ss)
+      cout << i << ' ';
+    cout << '\n';
   }
   return 0;
 }
