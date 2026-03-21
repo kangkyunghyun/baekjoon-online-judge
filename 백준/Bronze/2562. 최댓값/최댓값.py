cb2c860e9ce4c870e3fb6a262f97bd66f06d013d
강경현 <kangkh0906@gmail.com>
2026-03-22 01:51:46 +0900

    [Bronze III] Title: 최댓값, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,6 +1,17 @@
-num_list = []
-for i in range(9):
-    num_list.append(int(input()))
-
-print(max(num_list))
-print(num_list.index(max(num_list))+1)
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+g = int(input())
+h = int(input())
+i = int(input())
+iList = [a, b, c, d, e, f, g, h, i]
+M = iList[0]
+for i in range(len(iList)):
+    if iList[i] > M:
+        M = iList[i]
+cnt = iList.index(M)
+print(M)
+print(cnt+1)
