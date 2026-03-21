bf3f2ab28142e40127ac7af0ad3c98316348aa96
강경현 <kangkh0906@gmail.com>
2026-03-22 01:56:43 +0900

    [Bronze III] Title: 플러그, Time: 88 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2010. 플러그/플러그.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, sum = 0, c;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> c;
+    sum += c;
+  }
+  cout << sum - n + 1 << '\n';
+  return 0;
+}
