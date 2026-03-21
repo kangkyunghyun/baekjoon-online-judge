067dc6bcd76a6d2a1b3ca48db8412e87343737bc
강경현 <kangkh0906@gmail.com>
2026-03-22 02:30:10 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int k, q, r, v, n, p;
+  cin >> k >> q >> r >> v >> n >> p;
+  cout << 1 - k << ' ';
+  cout << 1 - q << ' ';
+  cout << 2 - r << ' ';
+  cout << 2 - v << ' ';
+  cout << 2 - n << ' ';
+  cout << 8 - p << '\n';
+}
