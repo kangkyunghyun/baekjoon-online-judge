7ff03c05c1fb0f60bf3d7b014e76dbc0166a9192
강경현 <kangkh0906@gmail.com>
2026-03-22 02:20:40 +0900

    [Bronze IV] Title: 영수증, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,12 @@
+X = int(input())
+N = int(input())
+
+result = 0
+for i in range(N):
+    a, b = map(int, input().split())
+    result += (a * b)
+
+if X == result:
+    print("Yes")
+else:
+    print("No")
