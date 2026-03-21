7d1607c1857326dd7761f160dc92d7d297fdcdfb
강경현 <kangkh0906@gmail.com>
2026-03-22 03:12:24 +0900

    [Silver III] Title: △, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/27966. △/△.cc
+++ 백준/Silver/27966. △/△.cc
@@ -7,7 +7,7 @@ int main() {
   long long n;
   cin >> n;
   cout << n - 1 + (n - 2) * (n - 1) << '\n';
-  for (int i = 2; i <= n; i++)
+  for (long long i = 2; i <= n; i++)
     cout << "1 " << i << '\n';
   return 0;
 }
