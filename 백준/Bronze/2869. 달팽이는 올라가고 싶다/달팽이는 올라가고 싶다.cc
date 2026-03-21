01beba530193b70f4cb156175419199146e2ba86
강경현 <kangkh0906@gmail.com>
2026-03-22 02:24:04 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b, v;
+  cin >> a >> b >> v;
+  if ((v - b) % (a - b) == 0) {
+    cout << (v - b) / (a - b) << '\n';
+  } else {
+    cout << (v - b) / (a - b) + 1 << '\n';
+  }
+  return 0;
+}
