5a929df4663f7c5358f9e667d14665aad85e7833
강경현 <kangkh0906@gmail.com>
2026-03-22 03:40:29 +0900

    [Bronze V] Title: Zadanie próbne, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  string s;
+  cin >> s;
+  reverse(s.begin(), s.end());
+  cout << s;
+  return 0;
+}
