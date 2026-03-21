b69a888b6809c7978294e6674a724bea112fbed0
강경현 <kangkh0906@gmail.com>
2026-03-22 03:23:18 +0900

    [Bronze IV] Title: ISBN, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6810. ISBN/ISBN.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << "The 1-3-sum is " << 91 + a + b * 3 + c;
+  return 0;
+}
