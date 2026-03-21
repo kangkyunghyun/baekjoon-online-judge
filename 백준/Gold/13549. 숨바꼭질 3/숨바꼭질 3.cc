d550dfeb776336ac11d612f2f853fe9563f9e3e6
강경현 <kangkh0906@gmail.com>
2026-03-22 03:04:39 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -1,34 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define INF 1e9
 
 int n, k;
-vector<int> visited(100001, INF);
+vector<int> visited(100001, -1);
 
-void bfs(int start) {
+void bfs(int s) {
   queue<int> q;
-  q.push(start);
-  visited[start] = 0;
+  q.push(s);
+  visited[s] = 0;
   while (!q.empty()) {
     int x = q.front();
     if (x == k)
       return;
     q.pop();
-    if (x * 2 >= 0 && x * 2 <= 100000)
-      if (visited[x * 2] == INF && visited[x * 2] > visited[x]) {
-        q.push(x * 2);
-        visited[x * 2] = visited[x];
-      }
-    if (x - 1 >= 0 && x - 1 <= 100000)
-      if (visited[x - 1] == INF && visited[x - 1] > visited[x] + 1) {
-        q.push(x - 1);
-        visited[x - 1] = visited[x] + 1;
-      }
-    if (x + 1 >= 0 && x + 1 <= 100000)
-      if (visited[x + 1] == INF && visited[x + 1] > visited[x] + 1) {
-        q.push(x + 1);
-        visited[x + 1] = visited[x] + 1;
-      }
+    if (x * 2 <= 100000 && visited[x * 2] == -1) {
+      q.push(x * 2);
+      visited[x * 2] = visited[x];
+    }
+    if (x - 1 >= 0 && visited[x - 1] == -1) {
+      q.push(x - 1);
+      visited[x - 1] = visited[x] + 1;
+    }
+    if (x + 1 <= 100000 && visited[x + 1] == -1) {
+      q.push(x + 1);
+      visited[x + 1] = visited[x] + 1;
+    }
   }
 }
 
