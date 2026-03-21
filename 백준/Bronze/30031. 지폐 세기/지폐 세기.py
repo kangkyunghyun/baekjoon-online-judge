77d122c3c4432680d79d7dbdb0debcc3ef3b6ee3
강경현 <kangkh0906@gmail.com>
2026-03-22 03:50:32 +0900

    [Bronze IV] Title: 지폐 세기, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/30031. 지폐 세기/지폐 세기.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+money = {136 : 1000, 142 : 5000, 148 : 10000, 154 : 50000}
+ans = 0
+for _ in range(int(input())):
+    ans += money[list(map(int, input().split()))[0]]
+print(ans)
