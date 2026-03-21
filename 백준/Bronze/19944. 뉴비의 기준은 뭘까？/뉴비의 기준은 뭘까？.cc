d1b409d711ded6be87932a851e07157b4e386b36
강경현 <kangkh0906@gmail.com>
2026-03-22 02:50:23 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  if (m == 1 || m == 2)
+    cout << "NEWBIE!";
+  else if (m <= n)
+    cout << "OLDBIE!";
+  else
+    cout << "TLE!";
+  return 0;
+}
