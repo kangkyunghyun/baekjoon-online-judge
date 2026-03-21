d3910e3b7e24d64c26e8c43eed6585c4d2709805
강경현 <kangkh0906@gmail.com>
2026-03-22 02:47:21 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL), cout.tie(NULL);
+  int a, b, c;
+  cin >> a >> b >> c;
+  if (a + b >= c * 2)
+    cout << a + b - c * 2;
+  else
+    cout << a + b;
+  return 0;
+}
