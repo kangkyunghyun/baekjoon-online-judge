27c99ea8cc66448d87975fd92896a5965d4d75a7
강경현 <kangkh0906@gmail.com>
2026-03-22 05:03:03 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL), cout.tie(NULL);
+  string s;
+  cin >> s;
+  cout << ":fan::fan::fan:\n";
+  cout << ":fan::" << s << "::fan:\n";
+  cout << ":fan::fan::fan:\n";
+  return 0;
+}
