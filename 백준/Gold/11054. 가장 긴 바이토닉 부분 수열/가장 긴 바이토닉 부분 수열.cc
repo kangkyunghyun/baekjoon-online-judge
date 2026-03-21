e488f7fc3d390892737b2977cd5c7ec70b2c11fe
강경현 <kangkh0906@gmail.com>
2026-03-22 03:04:37 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 220 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -4,21 +4,26 @@ using namespace std;
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(NULL);
-  int n, m = 0, ans;
+  int n, m = 0, ans = 0;
   cin >> n;
   vector<int> v(n), asc(n, 1), dsc(n, 1);
   for (int i = 0; i < n; i++)
     cin >> v[i];
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < i; j++)
-      if (v[i] > v[j])
+    for (int j = 0; j < i; j++) {
+      if (v[i] > v[j]) {
         asc[i] = max(asc[i], asc[j] + 1);
+      }
+    }
     if (asc[i] > m) {
       m = asc[i];
-      for (int a = i; a < n; a++)
-        for (int b = i; b < a; b++)
-          if (v[a] < v[b])
+      for (int a = i; a < n; a++) {
+        for (int b = i; b < a; b++) {
+          if (v[a] < v[b]) {
             dsc[a] = max(dsc[a], dsc[b] + 1);
+          }
+        }
+      }
       ans = max(ans, m + *max_element(dsc.begin(), dsc.end()) - 1);
     }
     fill(dsc.begin(), dsc.end(), 1);
