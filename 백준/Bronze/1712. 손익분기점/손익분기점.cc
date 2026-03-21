937a1526b7fd9df170f2f8383825e1ee82a74b3c
강경현 <kangkh0906@gmail.com>
2026-03-22 02:34:22 +0900

    [Bronze II] Title: 손익분기점, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B, C;
+  cin >> A >> B >> C;
+  if (B >= C)
+    cout << -1 << '\n';
+  else
+    cout << A / (C - B) + 1 << '\n';
+  return 0;
+}
