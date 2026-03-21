5a8472072ef94ae9a9df5c15d9e1fd061e01987f
강경현 <kangkh0906@gmail.com>
2026-03-22 03:17:46 +0900

    [Silver I] Title: OX, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

--- 백준/Silver/27970. OX/OX.cc
+++ 백준/Silver/27970. OX/OX.cc
@@ -1,25 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long power(long long x, long long y) {
-  if (y == 0)
-    return 1;
-  long long result = power(x, y / 2);
-  if (y % 2 == 0)
-    return result % 1000000007 * result % 1000000007;
-  else
-    return x * result % 1000000007 * result % 1000000007;
-}
-
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  long long ans = 0;
+  long long ans = 0, mul = 1, mod = 1e9 + 7;
   string s;
   cin >> s;
-  for (int i = 0; i < s.size(); i++)
+  for (int i = 0; i < s.size(); i++) {
     if (s[i] == 'O')
-      ans = ((ans % 1000000007) + (power(2, i) % 1000000007)) % 1000000007;
+      ans += mul;
+    ans %= mod;
+    mul *= 2;
+    mul %= mod;
+  }
   cout << ans;
   return 0;
 }
