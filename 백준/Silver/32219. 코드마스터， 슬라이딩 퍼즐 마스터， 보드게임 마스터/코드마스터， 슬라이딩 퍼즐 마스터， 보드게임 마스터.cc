fb18b13e97580ca05a41ccc661cdb967b657f12a
강경현 <kangkh0906@gmail.com>
2026-03-22 04:01:22 +0900

    [Silver III] Title: 코드마스터, 슬라이딩 퍼즐 마스터, 보드게임 마스터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/32219. 코드마스터， 슬라이딩 퍼즐 마스터， 보드게임 마스터/코드마스터， 슬라이딩 퍼즐 마스터， 보드게임 마스터.cc
+++ 백준/Silver/32219. 코드마스터， 슬라이딩 퍼즐 마스터， 보드게임 마스터/코드마스터， 슬라이딩 퍼즐 마스터， 보드게임 마스터.cc
@@ -13,17 +13,7 @@ signed main() {
             cout << "dohoon\n";
         else {
             cout << "jinseo\n";
-            if (r == 1)
-                cout << r << ' ' << c - 1;
-            else if (c == 1)
-                cout << r - 1 << ' ' << c;
-            else if (r == 2)
-                cout << r << " 2";
-            else if (c == 2)
-                cout << "2 " << c;
-            else
-                cout << min(r, c) << ' ' << min(r, c);
-            cout << '\n';
+            cout << min(r, c) << ' ' << min(r, c) << '\n';
         }
     }
 }
