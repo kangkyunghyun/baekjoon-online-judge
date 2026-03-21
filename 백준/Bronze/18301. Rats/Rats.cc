6d7cc184d460f04ec80af6d7d2f73172386f4bed
강경현 <kangkh0906@gmail.com>
2026-03-22 05:07:46 +0900

    [Bronze V] Title: Rats, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18301. Rats/Rats.cc
@@ -0,0 +1,10 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n1, n2, n12;
+  cin >> n1 >> n2 >> n12;
+  cout << floor((n1 + 1) * (n2 + 1) / (n12 + 1) - 1) << '\n';
+  return 0;
+}
