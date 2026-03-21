6996b98920627c19ac03fb94d7862160f1aefa68
강경현 <kangkh0906@gmail.com>
2026-03-22 02:52:36 +0900

    [Bronze III] Title: 더하기 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11023. 더하기 3/더하기 3.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL), cout.tie(NULL);
+  int ans = 0, x;
+  while (cin >> x)
+    ans += x;
+  cout << ans;
+  return 0;
+}
