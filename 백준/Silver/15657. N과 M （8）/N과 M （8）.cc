45c962042ad9c88a9c0a314132962c68de25608e
강경현 <kangkh0906@gmail.com>
2026-03-22 02:59:38 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> ans(8, 0), v;
+
+void dfs(int cnt) {
+  if (cnt == m) {
+    for (int i = 0; i < m; i++)
+      cout << ans[i] << ' ';
+    cout << '\n';
+    return;
+  }
+  for (int i = 0; i < n; i++)
+    if (v[i] >= ans[cnt - 1]) {
+      ans[cnt] = v[i];
+      dfs(cnt + 1);
+    }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL), cout.tie(NULL);
+  cin >> n >> m;
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    v.push_back(x);
+  }
+  sort(v.begin(), v.end());
+  dfs(0);
+  return 0;
+}
