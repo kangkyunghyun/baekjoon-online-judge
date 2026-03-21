3e9e8d22e0340de5873ad024d82dfb55622358cd
강경현 <kangkh0906@gmail.com>
2026-03-22 03:18:50 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 0 ms, Memory: 2392 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n;
+  string s;
+  cin >> n >> s;
+  cout << s.substr(n - 5, 5);
+  return 0;
+}
