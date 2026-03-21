d642e5b2546b63dcf2ff922f7ba5f23503e96577
강경현 <kangkh0906@gmail.com>
2026-03-22 02:41:42 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  while (1) {
+    vector<int> v(3);
+    cin >> v[0] >> v[1] >> v[2];
+    sort(v.begin(), v.end());
+    if (v[0] == 0 && v[1] == 0 && v[2] == 0)
+      break;
+    if (v[0] * v[0] + v[1] * v[1] == v[2] * v[2])
+      cout << "right\n";
+    else
+      cout << "wrong\n";
+  }
+  return 0;
+}
