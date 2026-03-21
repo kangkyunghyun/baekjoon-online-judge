0a26a4f23ca0587e72581484c789f7c419db7a42
강경현 <kangkh0906@gmail.com>
2026-03-22 02:57:20 +0900

    [Silver II] Title: N과 M (11), Time: 124 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> ans(8, 0), v, visited(8, 0);
+
+void dfs(int cnt) {
+  if (cnt == m) {
+    for (int i = 0; i < m; i++)
+      cout << ans[i] << ' ';
+    cout << '\n';
+    return;
+  }
+  int tmp = 0;
+  for (int i = 0; i < n; i++)
+    if (v[i] != tmp) {
+      ans[cnt] = v[i];
+      tmp = ans[cnt];
+      dfs(cnt + 1);
+    }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL), cout.tie(NULL);
+  cin >> n >> m;
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    v.push_back(x);
+  }
+  sort(v.begin(), v.end());
+  dfs(0);
+  return 0;
+}
