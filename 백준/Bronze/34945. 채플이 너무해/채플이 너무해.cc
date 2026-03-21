1cec0ee36975a0cf94624ddde9762b3ac3a2edf0
강경현 <kangkh0906@gmail.com>
2026-03-22 05:19:43 +0900

    [Bronze V] Title: 채플이 너무해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int x;
+    cin >> x;
+    cout << (x < 6 ? "Oh My God!" : "Success!");
+}
