795adad406a89044109564bb483a3d5f2807b065
강경현 <kangkh0906@gmail.com>
2026-03-22 02:47:28 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL), cout.tie(NULL);
+  vector<int> v(3);
+  for (int i = 0; i < 3; i++)
+    cin >> v[i];
+  sort(v.begin(), v.end());
+  for (int i : v)
+    cout << i << ' ';
+  return 0;
+}
