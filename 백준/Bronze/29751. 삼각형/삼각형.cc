d85eb90da4ce676603870f386d18d65646f57bfa
강경현 <kangkh0906@gmail.com>
2026-03-22 03:45:36 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  int w, h;
+  cin >> w >> h;
+  cout.precision(1);
+  cout << fixed;
+  cout << (double)w * h / 2;
+  return 0;
+}
