a3ff9255fefd23e15601dc2969b9e8d896b511ef
강경현 <kangkh0906@gmail.com>
2026-03-22 01:55:23 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, k, a, b;
+  cin >> n >> k;
+  for (int i = 0; i < n; i++) {
+    cin >> a >> b;
+  }
+  cout << "비와이" << '\n';
+  return 0;
+}
