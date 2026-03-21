affad3dd3938bbf81b24a05921525613127ca55d
강경현 <kangkh0906@gmail.com>
2026-03-22 02:42:25 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  int n, m, x;
+  cin >> n;
+  vector<int> v(n);
+  for (int i = 0; i < n; i++)
+    cin >> v[i];
+  sort(v.begin(), v.end());
+  cin >> m;
+  for (int i = 0; i < m; i++) {
+    cin >> x;
+    cout << binary_search(v.begin(), v.end(), x) << '\n';
+  }
+  return 0;
+}
