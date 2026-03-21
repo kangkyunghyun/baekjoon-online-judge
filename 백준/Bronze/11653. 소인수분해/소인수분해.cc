ce0a052d91c0e12e429fb5d00b97d30bc5556ac3
강경현 <kangkh0906@gmail.com>
2026-03-22 04:59:46 +0900

    [Bronze I] Title: 소인수분해, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  int n;
+  cin >> n;
+  for (int i = 2; n != 1; i++) {
+    while (n % i == 0) {
+      cout << i << '\n';
+      n /= i;
+    }
+  }
+  return 0;
+}
