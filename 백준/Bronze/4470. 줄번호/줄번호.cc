1c69d89d4ced20d8027d91d684a508581eb3f2f1
강경현 <kangkh0906@gmail.com>
2026-03-22 02:36:13 +0900

    [Bronze IV] Title: 줄번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  cin.ignore();
+  for (int i = 1; i <= n; i++) {
+    string str;
+    getline(cin, str);
+    cin.clear();
+    cout << i << ". " << str << '\n';
+  }
+  return 0;
+}
