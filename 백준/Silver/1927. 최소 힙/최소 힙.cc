027c5d3d1fb43ac41976169a21e4cf7e6ff57847
강경현 <kangkh0906@gmail.com>
2026-03-22 02:49:25 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL), cout.tie(NULL);
+  int n, x;
+  cin >> n;
+  priority_queue<int, vector<int>, greater<>> pq;
+  while (n--) {
+    cin >> x;
+    if (x == 0) {
+      if (pq.empty()) {
+        cout << 0 << '\n';
+      } else {
+        cout << pq.top() << '\n';
+        pq.pop();
+      }
+    } else {
+      pq.push(x);
+    }
+  }
+  return 0;
+}
