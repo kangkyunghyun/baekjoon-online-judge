2127f424e963016d06276539286528ecde0e437b
강경현 <kangkh0906@gmail.com>
2026-03-22 01:51:34 +0900

    [Bronze V] Title: A+B - 5, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -1,5 +1,5 @@
 while True:
-    a, b = map(int, input().split())
-    if a == 0 and b == 0:
+    A, B = map(int, input().split())
+    if A == 0 and B == 0:
         break
-    print(a+b)
+    print(A + B)
