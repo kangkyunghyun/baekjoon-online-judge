45d8eb29651411d1b2b089d954e9d8e31ef53351
강경현 <kangkh0906@gmail.com>
2026-03-22 03:43:09 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  char c;
+  cin >> c;
+  if (c == 'M')
+    cout << "MatKor";
+  else if (c == 'W')
+    cout << "WiCys";
+  else if (c == 'C')
+    cout << "CyKor";
+  else if (c == 'A')
+    cout << "AlKor";
+  else if (c == '$')
+    cout << "$clear";
+  return 0;
+}
