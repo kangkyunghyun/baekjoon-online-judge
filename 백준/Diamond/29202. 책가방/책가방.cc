4985581d5cbfc8de1547f8249d0ebb2389f96106
강경현 <kangkh0906@gmail.com>
2026-03-22 03:25:12 +0900

    [Diamond V] Title: 책가방, Time: 112 ms, Memory: 7492 KB, Score: 3 point -BaekjoonHub

---
+++ 백준/Diamond/29202. 책가방/책가방.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+ll n, k, ans = LLONG_MAX;
+struct Node {
+  ll w, v, t;
+};
+vector<int> bag, answer;
+int visited[200001] = {0};
+vector<Node> vec;
+
+void dfs(ll depth) {
+  if (depth >= k) {
+    ll S = 0, M = 0, m = LLONG_MAX;
+    for (int i : bag) {
+      S += vec[i].w;
+      M = max(M, vec[i].v);
+      m = min(m, vec[i].t);
+    }
+    if (ans > S + M + m) {
+      ans = S + M + m;
+      answer = bag;
+    }
+    return;
+  }
+  for (ll i = 0; i < n; i++) {
+    if (!visited[i]) {
+      visited[i] = 1;
+      bag.push_back(i);
+      dfs(depth + 1);
+      bag.pop_back();
+      visited[i] = 0;
+    }
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cin >> n >> k;
+  vec.resize(n);
+  for (ll i = 0; i < n; i++)
+    cin >> vec[i].w >> vec[i].v >> vec[i].t;
+  dfs(0);
+  cout << ans << '\n';
+  for (int i : answer)
+    cout << i + 1 << ' ';
+  return 0;
+}
