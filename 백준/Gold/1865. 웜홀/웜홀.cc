adb47b119e8fdf3b65ba883d5f39d3325177adff
강경현 <kangkh0906@gmail.com>
2026-03-22 03:01:46 +0900

    [Gold III] Title: 웜홀, Time: 40 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.cc
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -2,17 +2,36 @@
 using namespace std;
 #define INF 1e9
 
+int T, N, M, W;
+vector<pair<int, int>> graph[501];
+vector<int> dist(501, INF);
+bool minusCycle = false;
+
+void bellman_ford(int s) {
+  dist[s] = 0;
+  for (int i = 1; i <= N; i++)
+    for (int j = 1; j <= N; j++)
+      for (auto &p : graph[j]) {
+        int next = p.first, d = p.second;
+        if (dist[next] > dist[j] + d) {
+          dist[next] = dist[j] + d;
+          if (i == N)
+            minusCycle = true;
+        }
+      }
+}
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(NULL);
-  int T;
   cin >> T;
   while (T--) {
-    int N, M, W;
-    vector<pair<int, int>> graph[501];
-    vector<int> dist(501, INF);
-    bool minusCycle = false;
     cin >> N >> M >> W;
+    minusCycle = false;
+    for (int i = 1; i <= N; i++) {
+      dist[i] = INF;
+      graph[i].clear();
+    }
     while (M--) {
       int s, e, t;
       cin >> s >> e >> t;
@@ -24,18 +43,8 @@ int main() {
       cin >> s >> e >> t;
       graph[s].push_back({e, -t});
     }
-    dist[1] = 0;
-    for (int i = 1; i <= N; i++)
-      for (int j = 1; j <= N; j++)
-        for (auto &p : graph[j]) {
-          int next = p.first, d = p.second;
-          if (dist[next] > dist[j] + d) {
-            dist[next] = dist[j] + d;
-            if (i == N)
-              minusCycle = true;
-          }
-        }
-    cout << (minusCycle ? "YES\n" : "NO\n");
+    bellman_ford(1);
+    cout << (minusCycle || dist[1] < 0 ? "YES\n" : "NO\n");
   }
   return 0;
 }
