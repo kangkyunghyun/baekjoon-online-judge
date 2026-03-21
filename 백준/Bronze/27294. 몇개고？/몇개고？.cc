b8e0cf9d879df3635e5cbe8913f770e9535d93c8
강경현 <kangkh0906@gmail.com>
2026-03-22 02:01:52 +0900

    [Bronze V] Title: 몇개고?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL), cout.tie(NULL);
+  int t, s;
+  cin >> t >> s;
+  if (t >= 12 && t <= 16 && s == 0)
+    cout << 320;
+  else
+    cout << 280;
+  return 0;
+}
