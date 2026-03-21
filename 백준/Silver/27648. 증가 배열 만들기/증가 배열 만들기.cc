53e4640c70551479eda7c8bec187216cfc9c0314
강경현 <kangkh0906@gmail.com>
2026-03-22 03:06:58 +0900

    [Silver V] Title: 증가 배열 만들기, Time: 80 ms, Memory: 5812 KB -BaekjoonHub

--- 백준/Silver/27648. 증가 배열 만들기/증가 배열 만들기.cc
+++ 백준/Silver/27648. 증가 배열 만들기/증가 배열 만들기.cc
@@ -10,10 +10,14 @@ int main() {
     cout << "NO\n";
     return 0;
   }
+  int arr[1001][1001];
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      arr[i][j] = i + j + 1;
   cout << "YES\n";
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++)
-      cout << i + j + 1 << ' ';
+      cout << arr[i][j] << ' ';
     cout << '\n';
   }
   return 0;
