e8007f7ab87da02ff244cb71513429912ece79b8
강경현 <kangkh0906@gmail.com>
2026-03-22 01:55:20 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string a;
+  while (1) {
+    getline(cin, a);
+    if (a == "") {
+      break;
+    }
+    cout << a << '\n';
+  }
+  return 0;
+}
