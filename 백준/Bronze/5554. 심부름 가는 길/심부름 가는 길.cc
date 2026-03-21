ef158a3d6053b8cc66f1a2eb3e184d9d72c66f17
강경현 <kangkh0906@gmail.com>
2026-03-22 02:00:53 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL), cout.tie(NULL);
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  int sum = a + b + c + d;
+  cout << sum / 60 << '\n' << sum % 60;
+  return 0;
+}
