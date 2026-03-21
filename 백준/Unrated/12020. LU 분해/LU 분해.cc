32c68d9dc840c30c160a840c3c1ca4fda85d8743
강경현 <kangkh0906@gmail.com>
2026-03-22 03:06:33 +0900

    [Unrated] Title: LU 분해, Time: 540 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Unrated/12020. LU 분해/LU 분해.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  int n;
+  vector<vector<double>> L(1001, vector<double>(1001, 0)), U(1001, vector<double>(1001, 0));
+  cin >> n;
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++) {
+      cin >> U[i][j];
+      L[i][j] = i == j ? 1 : 0;
+    }
+  for (int i = 0; i < n - 1; i++) {
+    double d = U[i][i];
+    for (int j = i + 1; j < n; j++) {
+      if (i == j && U[i][j] == 0) {
+        cout << "-1\n";
+        return 0;
+      }
+      double x = U[j][i] / d;
+      for (int k = 0; k < n; k++)
+        U[j][k] -= x * U[i][k];
+      L[j][i] = x;
+    }
+  }
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      if (isnan(L[i][j]) || isnan(U[i][j])) {
+        cout << "-1\n";
+        return 0;
+      }
+  cout.precision(3);
+  cout << fixed;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++)
+      cout << L[i][j] << ' ';
+    cout << '\n';
+  }
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++)
+      cout << U[i][j] << ' ';
+    cout << '\n';
+  }
+  return 0;
+}
