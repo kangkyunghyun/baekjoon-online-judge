bd2402a567bcc7f572b9d13d3ae3cde9b240e0f6
강경현 <kangkh0906@gmail.com>
2026-03-22 15:06:39 +0900

    [Gold V] Title: 멍멍이 쓰다듬기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1669. 멍멍이 쓰다듬기/멍멍이 쓰다듬기.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL), cout.tie(NULL);
+  int x, y, ans = 0;
+  cin >> x >> y;
+  int gap = y - x, h = 1;
+  while (gap) {
+    gap -= 2 * h;
+    ans += 2;
+    if (gap <= h) {
+      while (gap != 0) {
+        if (gap < 0) {
+          gap += h;
+          ans--;
+          h--;
+        } else if (gap > 0) {
+          gap -= h;
+          ans++;
+          h++;
+        }
+      }
+    }
+    h++;
+  }
+  cout << ans << '\n';
+  return 0;
+}
