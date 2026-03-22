562881245d49f624956b116db6eaf0c3412c0a56
강경현 <kangkh0906@gmail.com>
2026-03-22 14:52:35 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 132 ms, Memory: 7404 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<vector<int>> v(n, vector<int>(2));
+  for (int i = 0; i < n; i++) {
+    cin >> v[i][1];
+    cin >> v[i][0];
+  }
+  sort(v.begin(), v.end());
+  for (int i = 0; i < n; i++)
+    cout << v[i][1] << ' ' << v[i][0] << '\n';
+  return 0;
+}
