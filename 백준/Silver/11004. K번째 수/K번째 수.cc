969842b7fcc5e7c7597690b2ebd08bed22edd9ec
강경현 <kangkh0906@gmail.com>
2026-03-22 14:52:23 +0900

    [Silver V] Title: K번째 수, Time: 1036 ms, Memory: 21552 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  int n, k;
+  cin >> n >> k;
+  vector<int> v(n);
+  for (int i = 0; i < n; i++)
+    cin >> v[i];
+  sort(v.begin(), v.end());
+  cout << v[k - 1];
+  return 0;
+}
