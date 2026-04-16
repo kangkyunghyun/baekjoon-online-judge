06181a383ae4df3aa56aef7679803eae4ee58a88
강경현 <kangkh0906@gmail.com>
2026-04-16 23:39:37 +0900

    [Bronze V] Title: 가희와 교통 요금, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/34665. 가희와 교통 요금/가희와 교통 요금.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+    string a, b;
+    cin >> a >> b;
+    cout << (a == b ? 0 : 1550);
+}
