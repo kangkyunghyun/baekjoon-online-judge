1f2f3250435799ad04d70ef60c6edaca6f5471f2
강경현 <kangkh0906@gmail.com>
2026-04-18 22:12:37 +0900

    [Bronze IV] Title: Acres, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26532. Acres/Acres.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+    double a, b;
+    cin >> a >> b;
+    cout << ceil(a * b / 4840 / 5);
+}
