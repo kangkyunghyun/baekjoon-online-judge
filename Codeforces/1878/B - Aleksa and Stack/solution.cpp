efe92b338629df4b7beb2ba59404c077e60254b9
강경현 <kangkh0906@gmail.com>
2026-04-30 01:48:14 +0900

    Add Aleksa and Stack [B] from Codeforces

---
+++ Codeforces/1878/B - Aleksa and Stack/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+      cout << i * 2 + 1 << ' ';
+    cout << '
+';
+  }
+  return 0;
+}
