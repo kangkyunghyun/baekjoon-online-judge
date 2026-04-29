94efcf3dcaf3580cd6129ff2153baa15399ef295
강경현 <kangkh0906@gmail.com>
2026-04-30 01:49:54 +0900

    Add Sublime Sequence [A] from Codeforces

---
+++ Codeforces/2148/A - Sublime Sequence/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+ 
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int x, n;
+        cin >> x >> n;
+        if (n % 2)
+            cout << x;
+        else
+            cout << 0;
+        cout << '
+';
+    }
+}
