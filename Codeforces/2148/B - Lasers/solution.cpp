be38f4edac0da81c71db88a4cf8c3dd2fab151f0
강경현 <kangkh0906@gmail.com>
2026-04-30 01:49:58 +0900

    Add Lasers [B] from Codeforces

---
+++ Codeforces/2148/B - Lasers/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+ 
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, x, y;
+        cin >> n >> m >> x >> y;
+        vector<int> a(n), b(m);
+        for (int &i : a)
+            cin >> i;
+        for (int &i : b)
+            cin >> i;
+        cout << n + m << '
+';
+    }
+}
