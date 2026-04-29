b0a7579bc690a860cb34d811af83676e2bea3870
강경현 <kangkh0906@gmail.com>
2026-04-30 01:46:38 +0900

    Add Subsequence Addition (Easy Version) [G1] from Codeforces

---
+++ Codeforces/1807/G1 - Subsequence Addition (Easy Version)/solution.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, flag = 0;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+      cin >> v[i];
+    sort(v.begin(), v.end());
+    if (v[0] != 1) {
+      cout << "NO
+";
+      continue;
+    }
+    string ans = "YES
+";
+    int s = 1;
+    for (int i = 1; i < n; i++) {
+      if (v[i] > s) {
+        ans = "NO
+";
+        break;
+      }
+      s += v[i];
+    }
+    cout << ans;
+  }
+  return 0;
+}
