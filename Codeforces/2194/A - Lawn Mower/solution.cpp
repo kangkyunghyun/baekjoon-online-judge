c709637b6bd3838433d2849b0f9df18da21e3dad
강경현 <kangkh0906@gmail.com>
2026-04-30 16:43:30 +0900

    Add Lawn Mower [A] from Codeforces

---
+++ Codeforces/2194/A - Lawn Mower/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+ 
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, w;
+        cin >> n >> w;
+        cout << n - n / w << '
+';
+    }
+}
