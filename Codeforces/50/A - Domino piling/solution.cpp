8360e7f6ce8c71da6563d56ea842c85f14c35036
강경현 <kangkh0906@gmail.com>
2026-05-02 00:00:16 +0900

    Add Domino piling [A] from Codeforces

---
+++ Codeforces/50/A - Domino piling/solution.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+ 
+signed main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int m, n;
+    cin >> m >> n;
+    cout << (m * n) / 2 << '
+';
+}
